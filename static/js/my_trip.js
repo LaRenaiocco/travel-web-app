d5aa116d247e72ef66ed4bb19a24ff00020c317f
LaRena Iocco <rosie.mcqueen@gmail.com>
2020-09-22 01:50:05 +0000

    clean up on my_trip files

--- static/js/my_trip.js
+++ static/js/my_trip.js
@@ -1,178 +1,168 @@
 "use strict"
 //  Get all itinerary, activity and note data from DB to render full itinerary.
 $.get('/users/itinerary/api', (data) => {
-
-    const response = JSON.parse(data)
-    const itinerary = response.itinerary
-    let activities = response.activities
-    const dates = response.dates
-    const notes = response.notes
-    const friends = response.friends
-    console.log(friends)
-
-    $('#trip-name').html(`${itinerary.trip_name}`);
-    $('#itinerary-id').html(`${itinerary.itinerary_id}`);
-
-    //  Sets up base itinerary calender based on days of trip.
-    let day = 1
-
-    dates.forEach(d => {
-
-        const date = document.createElement('div')
-        date.textContent = `day ${day}: ${d}`
-        date.setAttribute('id', d)
-        date.setAttribute('class', 'days')
-        $('#travel-dates').append(date)
-        day = day + 1
-    });
+  const response = JSON.parse(data);
+  const itinerary = response.itinerary;
+  let activities = response.activities;
+  const dates = response.dates;
+  const notes = response.notes;
+  const friends = response.friends;
+  // Fill in trip and itinerary id in HTML
+  $('#trip-name').html(`${itinerary.trip_name}`);
+  $('#itinerary-id').html(`${itinerary.itinerary_id}`);
+  //  Sets up itinerary calender based on days of trip.
+  let day = 1;
+  dates.forEach(d => {
+    const date = document.createElement('div');
+    date.textContent = `day ${day}: ${d}`;
+    date.setAttribute('id', d);
+    date.setAttribute('class', 'days');
+    $('#travel-dates').append(date);
+    day = day + 1;
+  });
     // Sorts activities by time(if one is included) for ordered 
     // placement on itinerary
-    const untimedActivities = []
-    const timedActivities = []
+    const untimedActivities = [];
+    const timedActivities = [];
+    // separate activities by timed and untimed and convert to iso format
     activities.forEach(a => {
         if (a.activity_time !== null) {
             if (a.activity_day !== null) {
-                a.iso = new Date(`${a.activity_day}T${a.activity_time}`)
+                a.iso = new Date(`${a.activity_day}T${a.activity_time}`);
             } else {
-            a.iso = new Date(`1970-01-01T${a.activity_time}`)}
-            timedActivities.push(a)
+            a.iso = new Date(`1970-01-01T${a.activity_time}`)};
+            timedActivities.push(a);
         } else {
-            untimedActivities.push(a)
-        }
-    })
-    timedActivities.sort((a, b) => a.iso - b.iso)
-    activities = timedActivities.concat(untimedActivities)
+            untimedActivities.push(a);
+        };
+    });
+    // sort timed activites in order from earliest to latest.
+    timedActivities.sort((a, b) => a.iso - b.iso);
+    // rejoin timed and untimed activities to one array
+    activities = timedActivities.concat(untimedActivities);
     // Adds activities to the appropriate day or to the bottom of the list 
     // if no day specified.
     activities.forEach(a => {
-
-        if (a.activity_day === null) {
-
-            const name = document.createElement('div')
-            name.setAttribute('class', 'activity-name')
-            name.textContent = a.activity_name
-            $('#misc-activities').append(name)
-
-            const address = document.createElement('div')
-            address.setAttribute('class', 'activity-address')
-            address.textContent = a.address
-            $('#misc-activities').append(address)
-
-            if (a.note !== null) {
-                const note = document.createElement('div')
-                note.setAttribute('class', 'activity-note')
-                note.textContent = a.activity_note
-                $('#misc-activities').append(note)
-            }
-        } else {
-            if (a.activity_time !== null) {
-                const time = document.createElement('div')
-                time.setAttribute('class', 'activity-time')
-                time.textContent = a.activity_time
-                $(`#${a.activity_day}`).append(time)
-            }
-
-            const name = document.createElement('div')
-            name.setAttribute('class', 'activity-name')
-            name.textContent = a.activity_name
-            $(`#${a.activity_day}`).append(name)
-
-            const address = document.createElement('div')
-            address.setAttribute('class', 'activity-address')
-            address.textContent = a.address
-            $(`#${a.activity_day}`).append(address)
-
-            if (a.activity_note !== null) {
-                const note = document.createElement('div')
-                note.setAttribute('class', 'activity-note')
-                note.textContent = a.activity_note
-                $(`#${a.activity_day}`).append(note)
-            }
-        }
-    })
+      // adds undated activity to misc. section of itinerary
+      if (a.activity_day === null) {
+        const name = document.createElement('div');
+        name.setAttribute('class', 'activity-name');
+        name.textContent = a.activity_name;
+        $('#misc-activities').append(name);
+        const address = document.createElement('div');
+        address.setAttribute('class', 'activity-address');
+        address.textContent = a.address;
+        $('#misc-activities').append(address);
+        // if an activity has an optional note, adds note to itinerary
+        if (a.note !== null) {
+          const note = document.createElement('div');
+          note.setAttribute('class', 'activity-note');
+          note.textContent = a.activity_note;
+          $('#misc-activities').append(note);
+        };
+      // adds dated activities to the itinerary
+      } else {
+        // if activity has a time, add time to itinerary
+        if (a.activity_time !== null) {
+          const time = document.createElement('div');
+          time.setAttribute('class', 'activity-time');
+          time.textContent = a.activity_time;
+          $(`#${a.activity_day}`).append(time);
+        };
+        const name = document.createElement('div');
+        name.setAttribute('class', 'activity-name');
+        name.textContent = a.activity_name;
+        $(`#${a.activity_day}`).append(name);
+        const address = document.createElement('div');
+        address.setAttribute('class', 'activity-address');
+        address.textContent = a.address;
+        $(`#${a.activity_day}`).append(address);
+        // if activity has a note, add note to itinerary
+        if (a.activity_note !== null) {
+          const note = document.createElement('div');
+          note.setAttribute('class', 'activity-note');
+          note.textContent = a.activity_note;
+          $(`#${a.activity_day}`).append(note);
+        };
+      };
+    });
     // Adds note to the speicied day or to the notes section
     // if no day specified.
     notes.forEach(n => {
-        if (n.day === null) {
-            const note = document.createElement('div')
-            note.setAttribute('class', 'itinerary-note')
-            note.textContent = `${n.comment} - ${n.author}`
-            $('#notes').append(note)
-        } else {
-            const note = document.createElement('div')
-            note.setAttribute('class', 'itinerary-note')
-            note.textContent = `${n.comment} - ${n.author}`
-            $(`#${n.day}`).append(note)
-        }
-    })
+      if (n.day === null) {
+        const note = document.createElement('div');
+        note.setAttribute('class', 'itinerary-note');
+        note.textContent = `${n.comment} - ${n.author}`;
+        $('#notes').append(note);
+      } else {
+        const note = document.createElement('div');
+        note.setAttribute('class', 'itinerary-note');
+        note.textContent = `${n.comment} - ${n.author}`;
+        $(`#${n.day}`).append(note);
+      };
+    });
     // Displays list of friends sharing this trip.
     friends.forEach(f => {
-        const friend = document.createElement('span')
-        friend.textContent = `${f[0]}   `
-        $('#travel-mates').append(friend)
-    })
-
+      const friend = document.createElement('span');
+      friend.textContent = `${f[0]}   `;
+      $('#travel-mates').append(friend);
+    });
     // Sets note date picker to days of trip.
-    const min = itinerary.start_date;
-    const max = itinerary.end_date;
-    
-    document.getElementById("comment-date").min = min
-    document.getElementById("comment-date").max = max
-})
-
-
-
+    document.getElementById("comment-date").min = itinerary.start_date;
+    document.getElementById("comment-date").max = itinerary.end_date;
+});
 
 //  Submits a new note to the DB and renders it to the Itinerary.
 $('#new-note-form').on('submit', (evt) => {
-    evt.preventDefault();
-
-    const formData = {
-        comment: $('#comment').val(),
-        date: $('#comment-date').val()
+  evt.preventDefault();
+  const formData = {
+    comment: $('#comment').val(),
+    date: $('#comment-date').val()
+  };
+  // reset new note form
+  document.getElementById("new-note-form").reset();
+  // send note to DB
+  $.post('/users/trips/new-note.json', formData, (data) => {
+    const response = JSON.parse(data);
+    const comment = response.comment;
+    const author = response.author;
+    const day = response.day;
+    // add  undated note to notes section 
+    if (day === null) {
+      const note = document.createElement('div');
+      note.setAttribute('class', 'itinerary-note');
+      note.textContent = `${comment} - ${author}`;
+      $('#notes').append(note);
+    // add note to applicable day
+    } else {
+      const note = document.createElement('div');
+      note.setAttribute('class', 'itinerary-note');
+      note.textContent = `${comment} - ${author}`;
+      $(`#${day}`).append(note);
     };
-
-    document.getElementById("new-note-form").reset();
-
-    $.post('/users/trips/new-note.json', formData, (data) => {
-        
-        const response = JSON.parse(data)
-        const comment = response.comment
-        const author = response.author
-        const day = response.day
-
-        if (day === null) {
-            const note = document.createElement('div')
-            note.setAttribute('class', 'itinerary-note')
-            note.textContent = `${comment} - ${author}`
-            $('#notes').append(note)
-        } else {
-            const note = document.createElement('div')
-            note.setAttribute('class', 'itinerary-note')
-            note.textContent = `${comment} - ${author}`
-            $(`#${day}`).append(note)
-        }
-    })
-})
+  });
+});
 
 //  Alerts user of Itinerary ID to link another user to the same itinerary.
 $('#add-mate-btn').on('click', () => {
-    const itinAlert = $('#itinerary-id').text()
-    $('#add-friend-modal-text').text(`Please give your travel mate the following id number to link your trips: ${itinAlert}. They will enter this unique id number in their profile to join this trip.`)
-    $('#add-friend-modal').modal('toggle')
-})
+  const itinAlert = $('#itinerary-id').text();
+  $('#add-friend-modal-text').text
+    (`Please give your travel mate the following id number to link your trips: ${itinAlert}. 
+    They will enter this unique id number in their profile to join this trip.`);
+  $('#add-friend-modal').modal('toggle');
+});
 
 //  redirects to the activity search page.
 $('#add-activity-btn').on('click', () => {
-    document.location.href = '/users/trips/activities'
-})
+  document.location.href = '/users/trips/activities';
+});
+
 //  Changes CSS for pretty printing
 window.onbeforeprint = function() {
-    $('#trip-name').css('text-align', 'left')
-    $('#col-two').hide()
-}
-
+  $('#trip-name').css('text-align', 'left');
+  $('#col-two').hide();
+};
 window.onafterprint = function() {
-    $('#trip-name').css('text-align', 'center')
-    $('#col-two').show()
-}
+  $('#trip-name').css('text-align', 'center');
+  $('#col-two').show();
+};
--- static/js/my_trip_map.js
+++ static/js/my_trip_map.js
@@ -1,18 +1,15 @@
 "use strict"
-const latLngObject = {lat: 0, lng: 0}
 
-//  Get itinerary and activity information from database
-
-
+const latLngObject = {lat: 0, lng: 0};
 // create map based on itinerary and activity information in DB.
 function initMap() {
+  //  Get itinerary and activity information from database
+  $.get('/users/trips/api', (data) => {
+    const response = JSON.parse(data);
+    latLngObject['lat'] = response.itinerary.lat;
+    latLngObject['lng'] = response.itinerary.lng;
+    const activities = response.activities
     // create map
-    $.get('/users/trips/api', (data) => {
-      const response = JSON.parse(data)
-    
-      latLngObject['lat'] = response.itinerary.lat;
-      latLngObject['lng'] = response.itinerary.lng;
-
     const basicMap = new google.maps.Map(
         document.querySelector('#itin-map'),
         {
@@ -21,14 +18,8 @@ function initMap() {
           mapTypeControl: false,
           fullscreenControl: false,
           styles: myMapStyle
-        }
-      );
-    // Check out the section on "Destructuring objects" here:
-    // https://hacks.mozilla.org/2015/05/es6-in-depth-destructuring/
-    const activities = response.activities
-
-      // Since you only use each of these variables once,
-      // consider passing them directly to the Marker constructor
+        });
+    // create map markers for each activity
     activities.forEach(a => {
       const id = a.activity_id;
       const name = a.activity_name;
@@ -41,38 +32,35 @@ function initMap() {
           map: basicMap
           }
       );
-      // marker info content
+      // marker info content: place name and address
       const infoContent = document.createElement('div');
       const placeName = document.createElement('div');
       placeName.setAttribute('class', 'marker-name');
-      placeName.textContent = name
+      placeName.textContent = name;
       infoContent.appendChild(placeName);
-      // infoContent.appendChild(document.createElement('br'));
-      
       const text = document.createElement('text');
       text.textContent = address
       infoContent.appendChild(text);
-
-      const activityInfo = new google.maps.InfoWindow({
-        content: infoContent
-      });
-      
+      // create info window attached to each marker
+      const activityInfo = new google.maps.InfoWindow({content: infoContent});
+      // set info window to open on click of marker
       activityMarker.addListener('click', function() {
           activityInfo.open(basicMap, activityMarker);
+          // find matching place on itinerary and highlight
           for (const a of document.getElementsByClassName('activity-name')) {
             if (a.textContent.includes(name)) {
-              // a.style.color = '#339989'
               a.setAttribute("style", "color: #8ae5d6; background-color: #339989; font-size: x-large")
-            }
-          }
-      })
+            };
+          };
+      });
+      // un-highlight on close of info window
       activityInfo.addListener('closeclick', function() {
         for (const a of document.getElementsByClassName('activity-name')) {
           if (a.textContent.includes(name)) {
             a.setAttribute("style", "color: #2d383e; background-color: #f6ddcb; font-size: large")
-          }
-        }
-      })
-    })
+          };
+        };
+      });
+    });
   });
-}
+};
